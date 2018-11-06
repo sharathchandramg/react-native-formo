8e753d5245806f5bb7dfd16647333196fee68fe3
rabindrachhanchan@gmail.com <rabindrachhanchan@gmail.com>
2018-11-06 18:49:57 +0530

    initial commit to support image

--- src/fields/image/index.js
+++ src/fields/image/index.js
@@ -1,4 +1,7 @@
-import React from "react";
+
+import PropTypes from "prop-types";
+import React,{Component} from "react";
+import {Icon} from "native-base";
 import {
 	ActionSheetIOS,
 	View,
@@ -10,7 +13,6 @@ import {
 
 import ImagePicker from "react-native-image-crop-picker";
 import BottomSheet from "react-native-js-bottom-sheet";
-import FontAwesome, { Icons } from 'react-native-fontawesome';
 import styles from "./styles";
 
 export default class ImageField extends Component {
@@ -88,12 +90,12 @@ export default class ImageField extends Component {
             {
 				title: options[0],
                 onPress: () => this._openCamera(),
-                icon: (<FontAwesome>{Icons.camera}</FontAwesome>)
+                icon: (<Icon name="ios-add" style={{fontSize: 20, color: 'red'}} size={24}/>)
 			},
 			{
 				title: options[1],
 				onPress: () =>this._openPicker(),
-				icon: ( <FontAwesome>{Icons.image}</FontAwesome>)
+				icon: ( <Icon name="ios-add" style={{fontSize: 20, color: 'red'}} size={24}/>)
 			}
 		];
 	};
@@ -128,8 +130,8 @@ export default class ImageField extends Component {
 					source={{ uri:attributes? attributes.value: this.state.path}}
 					style={[styles.image,{height: this.state.height}]}
 				/>
-				<View style={[styles.container,{overflow: this.state.overflow,backgroundColor: "#E28E8E"}]}>
-                    <FontAwesome>{Icons.camera}</FontAwesome>
+				<View style={[styles.container,{overflow: this.state.overflow}]}>
+                    <Icon name="ios-add" style={{fontSize: 20, color: 'red'}} size={24}/>
 				</View>
 			</TouchableOpacity>
 		);
@@ -140,24 +142,189 @@ export default class ImageField extends Component {
         const { theme, attributes, ErrorComponent } = this.props;
 
         return (
-            <View style={{height:150,justifyContent:'flex-start',alignItems:'center'}}>
-                <View style={{height:30,justifyContent:'flex-start',alignItems:'center'}}>
-                    <Text style={{ color: theme.inputColorPlaceholder }}>{attributes.label}</Text>
-                </View>
-                <View style={{height:100,justifyContent:'flex-start',alignItems:'center'}}>
-                    {this.renderPreview(attributes)}
-                </View>
+            <View>
+                <Text style={{ marginLeft:20,padding:5,color: theme.inputColorPlaceholder }}>{attributes.label}</Text>
+                {this.renderPreview(attributes)}
+
                 {Platform.OS === "android" ? (
                     <BottomSheet
                         ref={ref => {
                             this.bottomSheet = ref;
                         }}
-                        title={attributes.label}
+                        title={'Choose image from'}
                         options={this._renderOptions()}
                         coverScreen={true}
-                        titleFontFamily={style.titleFontFamily}
-                        styleContainer={style.styleContainer}
-                        fontFamily={style.fontFamily}
+                        titleFontFamily={styles.titleFontFamily}
+                        styleContainer={styles.styleContainer}
+                        fontFamily={styles.fontFamily}
+                    />
+                ) : null}
+                <ErrorComponent {...{ attributes, theme }} />
+            </View>
+        );
+    }
+}
+
+
+
+import PropTypes from "prop-types";
+import React,{Component} from "react";
+import {Icon} from "native-base";
+import {
+	ActionSheetIOS,
+	View,
+	Text,
+	Animated,
+	Platform,
+	TouchableOpacity
+} from "react-native";
+
+import ImagePicker from "react-native-image-crop-picker";
+import BottomSheet from "react-native-js-bottom-sheet";
+import styles from "./styles";
+
+export default class ImageField extends Component {
+
+    static propTypes = {
+        attributes: PropTypes.object,
+        theme: PropTypes.object,
+        updateValue: PropTypes.func,
+        ErrorComponent: PropTypes.func,
+    }
+
+    constructor(props) {
+		super(props);
+		this.state = {
+			path: undefined,
+			height: new Animated.Value(0),
+			overflow: "visible"
+		};
+	}
+
+    shouldComponentUpdate(nextProps, nextState) {
+		return true;
+	}
+
+    _startAnimation = () => {
+		Animated.sequence([
+			Animated.timing(this.state.height, {
+				toValue: 0,
+				duration: 250
+			}),
+			Animated.timing(this.state.height, {
+				toValue: 150,
+				duration: 500,
+				delay: 75
+			})
+		]).start();
+	};
+
+
+
+	_getImageFromStorage = (value) => {
+        this.setState({
+                path: value, 
+                overflow: "hidden" 
+            }, () =>this._startAnimation());
+        
+        this.props.updateValue(this.props.attributes.name, value);
+    };
+    
+    _openCamera =()=>{
+        ImagePicker.openCamera({
+            compressImageMaxWidth:360,
+            compressImageMaxHeight:360,
+            compressImageQuality: 1.0,
+            includeBase64: true,
+        }).then(image =>
+            this._getImageFromStorage(image.path)
+        ) && this.bottomSheet.close()
+    }
+    _openPicker =()=>{
+        ImagePicker.openPicker({
+            compressImageMaxWidth:360,
+            compressImageMaxHeight:360,
+            compressImageQuality: 1.0,
+            includeBase64: true,
+        }).then(image =>
+            this._getImageFromStorage(image.path)
+        ) && this.bottomSheet.close()
+    }
+
+    _renderOptions = () => {
+        const options =["Open camera","Select from the gallery","Cancel"];
+    
+		return [
+            {
+				title: options[0],
+                onPress: () => this._openCamera(),
+                icon: (<Icon name="ios-add" style={{fontSize: 20, color: 'red'}} size={24}/>)
+			},
+			{
+				title: options[1],
+				onPress: () =>this._openPicker(),
+				icon: ( <Icon name="ios-add" style={{fontSize: 20, color: 'red'}} size={24}/>)
+			}
+		];
+	};
+
+    _onPressImage = () => {
+		const options = this.props.options.config.options || this.defaultProps.options;
+		ActionSheetIOS.showActionSheetWithOptions({ options, cancelButtonIndex: 2},
+			buttonIndex => {
+				if (buttonIndex === 0) {
+					this._openCamera()
+				} else if (buttonIndex === 1) {
+					this._openPicker()
+				}
+			}
+		);
+	};
+
+
+
+
+
+    renderPreview =(attributes) => {
+		return (
+			<TouchableOpacity style={[styles.topContainer,{ borderColor:"#a94442" }]}
+				onPress={
+					Platform.OS === "ios"
+						? this._onPressImage
+						: () => this.bottomSheet.open()
+				}>
+				<Animated.Image
+					resizeMode="cover"
+					source={{ uri:attributes? attributes.value: this.state.path}}
+					style={[styles.image,{height: this.state.height}]}
+				/>
+				<View style={[styles.container,{overflow: this.state.overflow}]}>
+                    <Icon name="ios-add" style={{fontSize: 20, color: 'red'}} size={24}/>
+				</View>
+			</TouchableOpacity>
+		);
+    };
+
+    
+    render(){
+        const { theme, attributes, ErrorComponent } = this.props;
+
+        return (
+            <View>
+                <Text style={{ marginLeft:20,padding:5,color: theme.inputColorPlaceholder }}>{attributes.label}</Text>
+                {this.renderPreview(attributes)}
+
+                {Platform.OS === "android" ? (
+                    <BottomSheet
+                        ref={ref => {
+                            this.bottomSheet = ref;
+                        }}
+                        title={'Choose image from'}
+                        options={this._renderOptions()}
+                        coverScreen={true}
+                        titleFontFamily={styles.titleFontFamily}
+                        styleContainer={styles.styleContainer}
+                        fontFamily={styles.fontFamily}
                     />
                 ) : null}
                 <ErrorComponent {...{ attributes, theme }} />
--- src/fields/image/styles.js
+++ src/fields/image/styles.js
@@ -8,20 +8,20 @@ import {
 const styles = StyleSheet.create({
     topContainer: {
         overflow: 'hidden',
-        borderRadius: 4,
         marginBottom: 12,
         height: 150,
-        borderColor: 'grey',
-        borderWidth: 1
+        marginHorizontal:20,
+        
     },
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#e6e6e6',
-
         height: 100,
-        borderRadius: 4
+        borderRadius: 4,
+        paddingVertical: 10,
+        borderWidth:1,
+        borderColor: 'grey',
     },
 
     image: {
