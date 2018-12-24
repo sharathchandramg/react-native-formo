5273af026325fc1930130bcf4fea5123382c52c8
Rabindra Chhanchan <rabindrachhanchan@gmail.com>
2018-12-24 11:09:22 +0530

    added calucated feature (#17)
    * added calucated feature
    
    * resolved conflict

--- src/fields/textInput/index.js
+++ src/fields/textInput/index.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { Platform } from "react-native";
+import math from "mathjs"
 
 import { View, Item, Input, Icon, ListItem, Text } from "native-base";
 
@@ -24,12 +25,83 @@ export default class TextInputField extends Component {
         this.props.updateValue(this.props.attributes.name, text);
     }
 
+    getCalculatedValue = (attributes)=>{
+        let value = '' ;
+        if(typeof attributes.value !=='undefined' && attributes.value !== null){
+            return attributes.value.toString();
+        }else{
+            let calculateOnFields = attributes.fields;
+            let expression = attributes.expression;
+            if(typeof calculateOnFields ==='object'){
+                let scope = {};
+                for(let i = 0;i < calculateOnFields.length ; i++){
+                    scope[calculateOnFields[i]] = parseFloat(this.props.getValue(calculateOnFields[i]));
+                }
+                if(Object.values(scope).length === calculateOnFields.length){
+                    value = math.eval(expression , scope)
+                }
+            }
+            return isNaN(value)?'':value.toString();
+        }
+
+    }
+
+    renderCalculatedField =(attributes,theme)=>{
+        const inputProps = attributes.props;
+        const keyboardType = getKeyboardType(attributes.type);
+        return(
+            <Input
+                style={{
+                    height: inputProps && inputProps.multiline && (Platform.OS === 'ios' ? undefined : null),
+                    paddingStart:5,
+                }}
+                ref={(c) => { this.textInput = c; }}
+                keyboardType={keyboardType}
+                underlineColorAndroid="transparent"
+                numberOfLines={2}
+                placeholder={attributes.label}
+                placeholderTextColor={theme.inputColorPlaceholder}
+                editable={attributes.editable}
+                onChangeText={text => this.handleChange(text)}
+                // value={attributes.value}
+                value={this.getCalculatedValue(attributes)}
+                {...inputProps}
+            />
+        )
+    }
+
+    renderInputField = (attributes,theme) =>{
+        const inputProps = attributes.props;
+        const keyboardType = getKeyboardType(attributes.type);
+        return(
+            <Input
+                style={{
+                    height: inputProps && inputProps.multiline && (Platform.OS === 'ios' ? undefined : null),
+                    paddingStart:5,
+                }}
+                ref={(c) => { this.textInput = c; }}
+                keyboardType={keyboardType}
+                underlineColorAndroid="transparent"
+                numberOfLines={2}
+                secureTextEntry={attributes.secureTextEntry || attributes.type === "password"}
+                placeholder={attributes.label}
+                blurOnSubmit={false}
+                placeholderTextColor={theme.inputColorPlaceholder}
+                editable={attributes.editable}
+                onChangeText={text => this.handleChange(text)}
+                // value={attributes.value}
+                value={attributes.value && attributes.value.toString()}
+                {...inputProps}
+            />
+        )
+
+    }
+
+
     render() {
 
         const { theme, attributes, ErrorComponent } = this.props;
-        const inputProps = attributes.props;
-        const keyboardType = getKeyboardType(attributes.type);
-
+    
         return (
 
             <ListItem style={{ borderBottomWidth: 0, paddingVertical: 5 }}>
@@ -37,25 +109,11 @@ export default class TextInputField extends Component {
                     <View>
                         <Item error={theme.changeTextInputColorOnError ? attributes.error : null}>
                             {attributes.icon && <Icon color={theme.textInputIconColor} name={attributes.icon} />}
-                            <Input
-                                style={{
-                                    height: inputProps && inputProps.multiline && (Platform.OS === 'ios' ? undefined : null),
-                                    paddingStart:5,
-                                }}
-                                ref={(c) => { this.textInput = c; }}
-                                keyboardType={keyboardType}
-                                underlineColorAndroid="transparent"
-                                numberOfLines={2}
-                                secureTextEntry={attributes.secureTextEntry || attributes.type === "password"}
-                                placeholder={attributes.label}
-                                blurOnSubmit={false}
-                                placeholderTextColor={theme.inputColorPlaceholder}
-                                editable={attributes.editable}
-                                onChangeText={text => this.handleChange(text)}
-                                // value={attributes.value}
-                                value={attributes.value && attributes.value.toString()}
-                                {...inputProps}
-                            />
+                            {typeof attributes.type  !=='undefined' && attributes.type ==="calculated"?
+                                this.renderCalculatedField(attributes,theme) 
+                                :
+                                this.renderInputField(attributes,theme)
+                            }
                             {theme.textInputErrorIcon && attributes.error ? <Icon name={theme.textInputErrorIcon} /> : null}
                         </Item>
                     </View>
