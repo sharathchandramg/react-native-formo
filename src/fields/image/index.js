b351240173f4c44c629680d12d85e7872a356bac
Nitheesh Salian <nitheesh@skiploop.com>
2018-12-26 00:36:41 +0530

    IOS options list (#16)
    * IOS options list
    
    * picker options from json

--- src/fields/image/index.js
+++ src/fields/image/index.js
@@ -112,7 +112,7 @@ export default class ImageField extends Component {
 	};
 
     _onPressImage = () => {
-		const options = this.props.options.config.options || this.defaultProps.options;
+		const options = this.props.attributes.options;
 		ActionSheetIOS.showActionSheetWithOptions({ options, cancelButtonIndex: 2},
 			buttonIndex => {
 				if (buttonIndex === 0) {
