b7d05373ea6dbfb9c01cde95e363ddf96c15c913
Sharath Chandra <sharath@skiploop.com>
2018-12-28 17:04:00 +0530

    Updating the helper functions for default datetime

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -89,7 +89,6 @@ export function getDefaultValue(field) {
                         else if (field.defaultValue === "tomorrow")
                             return moment().add(1, "day").format("YYYY-MM-DD HH:MM");
                         else if (typeof field.defaultValue !== "undefined" && Number.parseInt(field.defaultValue) >= 0) {
-                            console.log("ehere");
                             let totalMinutes = Number.parseInt(field.defaultValue);
                             return moment().add(totalMinutes, "minutes").format("YYYY-MM-DD HH:MM");
                         }
