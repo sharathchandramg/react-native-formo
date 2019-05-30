5a006d19d050b16552bd8b758328bd674192a0cd
Rabindra Chhanchan <rabindrachhanchan@Apples-MacBook-Pro.local>
2019-05-30 14:41:56 +0530

    lookup item view

--- src/components/recyclerList/index.js
+++ src/components/recyclerList/index.js
@@ -4,10 +4,12 @@ import {
     LayoutProvider,
     DataProvider,
 } from 'recyclerlistview';
-import { Text, ListItem, CheckBox, Body, View } from 'native-base';
+import { Text, ListItem, CheckBox, Body, View, Left, Right } from 'native-base';
 import { Dimensions } from 'react-native';
 import { isNull } from '../../utils/validators';
 import styles from './styles';
+
+import Item from '../lookupItem';
 
 export default class RecyclerList extends Component {
     constructor(props) {
@@ -22,7 +24,7 @@ export default class RecyclerList extends Component {
             },
             (type, dim) => {
                 dim.width = width;
-                dim.height = 35;
+                dim.height = 55;
             }
         );
 
@@ -35,35 +37,19 @@ export default class RecyclerList extends Component {
             if (this.props.attributes.multiple) {
                 isSelected = this.props.attributes.objectType
                     ? this.props.attributes.value.findIndex(
-                        option =>
-                            option[this.props.attributes.primaryKey] ===
-                            item[this.props.attributes.primaryKey]
-                    ) !== -1
+                          option =>
+                              option[this.props.attributes.primaryKey] ===
+                              item[this.props.attributes.primaryKey]
+                      ) !== -1
                     : this.props.attributes.value.indexOf(item) !== -1;
             }
             return (
-                <ListItem
-                    key={
-                        this.props.attributes.objectType &&
-                        item[this.props.attributes.labelKey]
-                    }
-                    onPress={() => this.props.toggleSelect(item)}
-                    style={styles.labelContainer}
-                >
-                    {this.props.attributes.multiple && (
-                        <CheckBox
-                            onPress={() => this.props.toggleSelect(item)}
-                            checked={isSelected}
-                        />
-                    )}
-                    <Body>
-                        <Text style={{ paddingHorizontal: 5 }}>
-                            {this.props.attributes.objectType
-                                ? item[this.props.attributes.labelKey]
-                                : item}
-                        </Text>
-                    </Body>
-                </ListItem>
+                <Item
+                    toggleSelect={this.props.toggleSelect}
+                    attributes={this.props.attributes}
+                    isSelected={isSelected}
+                    item={item}
+                />
             );
         }
         return null;
@@ -71,7 +57,7 @@ export default class RecyclerList extends Component {
 
     render() {
         return (
-            <View style={{ minHeight:30, width: '100%', marginVertical: 10 }}>
+            <View style={{ minHeight: 60 }}>
                 <RecyclerListView
                     layoutProvider={this._layoutProvider}
                     dataProvider={this.dataProvider.cloneWithRows(
@@ -81,6 +67,7 @@ export default class RecyclerList extends Component {
                     canChangeSize={true}
                     onEndReached={this.props.onEndReached}
                     onEndReachedThreshold={0.1}
+                    style={{ margin: 10 }}
                 />
             </View>
         );
