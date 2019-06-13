69c008405f1d41838a737d2c006d4e8fe32ec1e5
Rabindra Chhanchan <rabindrachhanchan@gmail.com>
2019-06-13 17:05:28 +0530

    added required icon

---
+++ src/components/starIcon/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+    iconWrapper:{
+        height: 20,
+        width: 20,
+        borderRadius:10,
+    },
+    iconStyle:{
+        fontSize:15, 
+        color: "#FFB646", 
+        lineHeight: 20
+    }
+})
+export default styles
