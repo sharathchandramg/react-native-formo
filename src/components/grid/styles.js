7a5ff96675ffc97c6249a246d4974845a0a69609
Rabindra Chhanchan <rabindrachhanchan@gmail.com>
2019-07-17 13:06:18 +0530

    change input line height

--- src/components/grid/styles.js
+++ src/components/grid/styles.js
@@ -1,158 +1,170 @@
 import {
-    StyleSheet,
-    Dimensions
+  StyleSheet,
+  Dimensions
 } from "react-native";
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding:10,
-    },
-    header: {
-      height:40,
-      backgroundColor: '#537791'
-    },
-    text: {
-      textAlign: 'center',
-      fontWeight: '100'
-    },
-    dataWrapper: {
-      marginTop: -1,
-    },
-
-    row: {
-      height:40,
-      flexDirection: "row",
-      overflow: "hidden",
-      // backgroundColor:'#E1FBFF',
-    },
-
-    col:{
-      // borderRightWidth:0.5,
-      // borderBottomWidth:0.5,
-      borderColor:'#BDCCDB',
-      overflow: "hidden"
-  
-    },
-    cell:{
-      height:40,
-      width:100,
-      borderColor:'#BDCCDB',
-      borderTopWidth:0.5,
-      borderRightWidth:0.5,
-      borderStyle:'solid',
-      justifyContent:'center',
-      alignItems:'center',
-      // backgroundColor: '#E7E6E1',
-    },
-    cellTextBox:{
-      height:40,
-      width:100,
-      borderColor:'#BDCCDB',
-      borderTopWidth:0.5,
-      borderRightWidth:0.5,
-      borderStyle:'solid',
-      justifyContent:'center',
-      alignItems:'center',
-  
-    },
-    wrapper: {
-      flexDirection: 'row',
-      borderLeftWidth:0.5,
-      borderBottomWidth:0.5,
-      borderStyle: 'solid',
-      borderColor:'#BDCCDB',
-    },
-    tableStyle:{
-      borderLeftWidth:0.5,
-      borderBottomWidth:0.5,
-      borderStyle: 'solid',
-      borderColor:'#BDCCDB',
-    },
-
-    textBox:{
-      fontFamily: 'roboto',
-      fontSize:14,
-      fontWeight: '400',
-      lineHeight: 24,
-      textAlign:'center',
-      height:30,
-      width:'100%',
-      color: 'black',
-    },
-    inputBoxWrapper:{
-      width:100,
-      // height:40,
-      marginBottom:10,
-      marginTop:5,
-      alignItems:'center',
-      justifyContent:'center',
-    },
-    inputBox:{
-      fontFamily: 'roboto',
-      fontSize:12,
-      fontWeight: '300',
-      lineHeight:20,
-      width:'100%',
-      textAlign:'center',
-      color: '#989898', 
-    },
-
-    footer:{
-      height:150,
-      width:'100%',
-      position:'absolute',
-      bottom: 0,
-      backgroundColor:'white',
-      borderTopWidth: 0.5,
-      borderColor: '#d6d7da',
-    },
-    button: {
-      height: 50,
-      width:'100%',
-      backgroundColor: '#48BBEC',
-      bottom: 0,
-      position:'absolute',
-      alignSelf: 'stretch',
-      justifyContent: 'center'
+  container: {
+    flex: 1,
+    padding:10,
   },
-  buttonText:{
-    fontFamily: 'roboto',
-    fontSize:16,
-    fontWeight: 'bold',
-    color: 'white',
-    lineHeight: 24,
-    textAlign:'center',
+  header: {
+    height:40,
+    backgroundColor: '#537791'
   },
-  aggregateWrapper: {
-    height: 100,
-    width: '100%',
+  text: {
+    textAlign: 'center',
+    fontWeight: '100'
+  },
+  dataWrapper: {
+    marginTop: -1,
+  },
+
+  row: {
+    height:40,
+    flexDirection: "row",
+    overflow: "hidden",
+    // backgroundColor:'#E1FBFF',
+  },
+
+  col:{
+    // borderRightWidth:0.5,
+    // borderBottomWidth:0.5,
+    borderColor:'#BDCCDB',
+    overflow: "hidden"
+
+  },
+  cell:{
+    height:40,
+    width:100,
+    borderColor:'#BDCCDB',
+    borderTopWidth:0.5,
+    borderRightWidth:0.5,
+    borderStyle:'solid',
+    justifyContent:'center',
+    alignItems:'center',
+    // backgroundColor: '#E7E6E1',
+  },
+  cellTextBox:{
+    height:40,
+    width:100,
+    borderColor:'#BDCCDB',
+    borderTopWidth:0.5,
+    borderRightWidth:0.5,
+    borderStyle:'solid',
+    justifyContent:'center',
+    alignItems:'center',
+
+  },
+  wrapper: {
     flexDirection: 'row',
-    borderColor: '#d6d7da',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderTopWidth: 0.5,
-    position:'absolute',
-    bottom:50,
-    left:0
+    borderLeftWidth:0.5,
+    borderBottomWidth:0.5,
+    borderStyle: 'solid',
+    borderColor:'#BDCCDB',
   },
-  aggregateTextWrapper:{
-    height:80,
-    width:'80%',
-    justifyContent: 'center',
-    alignItems: 'center',
+  tableStyle:{
+    borderLeftWidth:0.5,
+    borderBottomWidth:0.5,
+    borderStyle: 'solid',
+    borderColor:'#BDCCDB',
   },
-  summaryText:{
+
+  textBox:{
     fontFamily: 'roboto',
     fontSize:14,
-    fontWeight: '300',
-    color: '#989898',
-    lineHeight: 24,
+    fontWeight: '400',
+    lineHeight:16,
+    height:16,
+    width:'100%',
+    color: 'black',
+    paddingVertical:0,
+    textAlignVertical: 'center',
     textAlign:'center',
+    alignItems:'center',
+    justifyContent:'center',
+    alignSelf:'center',
+  },
+  inputBoxWrapper:{
+    width:100,
+    alignItems:'center',
+    justifyContent:'center',
+    alignSelf:'center',
+  },
+  inputBox:{
+    fontFamily: 'roboto',
+    fontSize:12,
+    flexDirection:'row',
+    textAlign:'center',
+    alignSelf:'center',
+    width:100,
+    height:16,
+    lineHeight:16,
+    paddingVertical:0,
+    textAlignVertical: 'center',
+    alignItems:'center',
+    justifyContent:'center',
+    lineHeight:14,
+    fontWeight: '300',
+    width:'100%',
+    color: '#989898', 
+  },
 
-  }
-  
-  });
-  
+  footer:{
+    height:150,
+    width:'100%',
+    position:'absolute',
+    bottom: 0,
+    backgroundColor:'white',
+    borderTopWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  button: {
+    height: 50,
+    width:'100%',
+    backgroundColor: '#48BBEC',
+    bottom: 0,
+    position:'absolute',
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+},
+buttonText:{
+  fontFamily: 'roboto',
+  fontSize:16,
+  fontWeight: 'bold',
+  color: 'white',
+  lineHeight: 24,
+  textAlign:'center',
+},
+aggregateWrapper: {
+  height: 100,
+  width: '100%',
+  flexDirection: 'row',
+  borderColor: '#d6d7da',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderTopWidth: 0.5,
+  position:'absolute',
+  bottom:50,
+  left:0
+},
+aggregateTextWrapper:{
+  height:80,
+  width:'80%',
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+summaryText:{
+  fontFamily: 'roboto',
+  fontSize:14,
+  fontWeight: '300',
+  color: '#989898',
+  lineHeight: 24,
+  textAlign:'center',
+
+}
+
+});
+
 export default styles;
