518f1d547e833a32ff311df145b29c17b8193b08
Rabindra Chhanchan <rabindrachhanchan@gmail.com>
2019-11-12 13:29:23 +0530

    Refactor simple grid (#98)
    * refactor simple grid ui
    
    * reset field
    
    * refactor ui

--- src/components/grid/cols.js
+++ src/components/grid/cols.js
@@ -3,28 +3,26 @@ import { View } from 'react-native';
 import { Cell } from './cell';
 
 export const Col = props => {
-	const { data, theme, wth,height } = props;
+	const { data,wth,height } = props;
 	return data ? (
 		<View >
-		{data.map((item, i) => {
-			return (
-			<View style={{backgroundColor: i%2 === 0 ?'#E1FBFF':'white',color: '#989898'}}>
-				<Cell
-					rowKey={item['rowKey']}
-					colKey={item['colKey']}
-					type={item['type']}
-					value={item['value']}
-					key={i}
-					editable={item['editable']}
-					theme={theme}
-					width={wth}
-					height={height}
-					color= {'black'}
-				/>
-			</View>		
-			
-			);
-		})}
+			{data.map((item, i) => {
+				return (
+				<View 
+					key={`${item['rowKey']}`}
+					style={{backgroundColor: i%2 === 0 ?'#E1FBFF':'white',color: '#989898'}}>
+					<Cell
+						type={item['type']}
+						value={item['value']}
+						width={wth}
+						height={height}
+						color= {'black'}
+						keyIndex={`${item['rowKey']}`}
+					/>
+				</View>		
+				
+				);
+			})}
 		</View>
 	) : null;
 }
---
+++ src/components/grid/editComponent.js
@@ -0,0 +1,92 @@
+
+import React from "react";
+import {
+	TouchableOpacity,
+    View,
+    Text,
+    Platform
+} from "react-native";
+
+import {
+	Container,
+	Content,
+    Footer,
+    Item,
+    Input,
+    Title
+
+} from "native-base";
+import CustomHeader from '../headers/hearder'
+import styles from "./styles";
+import {isEmpty} from "../../utils/validators";
+
+const EditComponent = props => {
+	const { 
+        theme,
+        rowData,
+        onChangeText,
+        handleOnSaveClick,
+    } = props
+
+    
+	renderItemList =(rowData)=>{
+        return rowData ? (
+            <View style={{ borderBottomWidth: 0, paddingVertical: 5 }} >
+                <View style={{marginVertical:20,marginHorizontal:10}}>
+                    <Title style={styles.topLabel}> 
+                        {rowData[0]['rowKey']||''}
+                    </Title>
+                </View>
+                
+                {rowData.map((item, i) =>{
+                    const keyboardType =  item['type'] && item['type'].toLowerCase()==='number'? 'numeric': 'default';
+                    const editable = !item['editable'] ? item['editable'] : true;
+                    return(
+                        <View key ={`${item['rowKey']+""+item['colKey']}`} style={{flex:1,marginBottom:5}}>
+                            <Item style={{height:50}}>
+                                <Input
+                                    style={{
+                                        height:Platform.OS === 'ios' ? undefined : null,
+                                        paddingStart:5,
+                                    }}
+                                    underlineColorAndroid="transparent"
+                                    numberOfLines={5}
+                                    maxLength={100}
+                                    multiline={true}
+                                    placeholder={item['colKey']||'None'}
+                                    placeholderTextColor={theme.inputColorPlaceholder}
+                                    onChangeText={text => onChangeText(item['rowKey'] ,item['colKey'],text)}
+                                    value={item['value']}
+                                    keyboardType={keyboardType}
+                                    editable={editable}
+                                
+                                />
+                            </Item>
+                        </View>
+                    )})
+                } 
+            </View>
+        ):null
+	}
+
+	return (
+		<Container style={{ flex: 1 }}>
+				<CustomHeader {...props } />
+				<Content style={{marginBottom:50}}>
+					<View style={styles.container}>
+						{!isEmpty(rowData) && Array.isArray(rowData)? renderItemList(rowData): null}
+					</View>
+				</Content>
+				<Footer style={[styles.footer,{borderTopWidth:0, height:50,}]}>
+					<TouchableOpacity style={styles.button} onPress={() => handleOnSaveClick()}>
+						<Text style={styles.buttonText}>{'SAVE'} </Text>
+					</TouchableOpacity>
+                </Footer>
+			</Container>
+		
+	);
+};
+
+export default EditComponent;
+
+
