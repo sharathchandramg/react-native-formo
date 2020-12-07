9d8cf68a33426b699c2f4a0e4a5c1a61e5f7d8be
KKrupanand <41726447+Kkrupanand@users.noreply.github.com>
2020-12-07 19:18:07 +0530

    Update search UI (#138)
    Co-authored-by: Krupanand <krupanand@mobileoffize.com>

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -2,23 +2,53 @@ import React from 'react';
 import SearchHeader from '../headers/searchHeader';
 import { Container, Content } from 'native-base';
 import RecyclerList from '../recyclerList';
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { Icon } from 'native-base';
 import { isNull } from '../../utils/validators';
 import styles from './styles';
 
 const SearchComponent = props => {
+    const {
+        handleOnSearchQuery,
+        theme,
+        searchText,
+        attributes,
+        onEndReached,
+        toggleSelect,
+    } = props;
     return (
         <Container style={{ flex: 1 }}>
             <SearchHeader {...props} />
             <Content>
                 <View style={{ height: '100%', width: '100%' }}>
-                    {!isNull(props.attributes['options']) &&
-                    props.attributes['options'].length ? (
+                    {searchText.length > 0 ? (
+                        <View style={styles.searchForWrapper}>
+                            <TouchableOpacity
+                                onPress={() =>
+                                    handleOnSearchQuery(searchText, false)
+                                }
+                                style={styles.searchForButton}
+                            >
+                                <Icon
+                                    name="search"
+                                    type="FontAwesome"
+                                    style={[
+                                        theme.headerLeftIcon,
+                                        { fontSize: 18, paddingRight: 20 },
+                                    ]}
+                                />
+                                <Text style={styles.searchForText}>
+                                    {`Search for "${searchText}"`}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    ) : !isNull(attributes['options']) &&
+                      attributes['options'].length ? (
                         <RecyclerList
-                            dataProvider={props.attributes['options']}
-                            onEndReached={props.onEndReached}
-                            attributes={props.attributes}
-                            toggleSelect={props.toggleSelect}
+                            dataProvider={attributes['options']}
+                            onEndReached={onEndReached}
+                            attributes={attributes}
+                            toggleSelect={toggleSelect}
                         />
                     ) : (
                         <ScrollView
