6f32312c0617dfb3e3bce4c03cf937b73cbc1207
krupanand <krupa@krupas-MacBook-Pro.local>
2021-10-15 00:32:50 +0530

    Added no records found message for user directory, if it has empty data

--- src/fields/userDirectory/styles.js
+++ src/fields/userDirectory/styles.js
@@ -137,6 +137,25 @@ const styles = StyleSheet.create({
         width: 30,
         marginEnd: 10,
     },
+
+    noDataWrapper: {
+        height: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    nodataText: {
+        height: 30,
+        lineHeight: 20,
+        color: '#989898',
+        fontFamily: 'roboto',
+        width: '100%',
+        fontSize: 14,
+        padding: 5,
+        fontWeight: '700',
+        alignSelf: 'center',
+        textAlign: 'center',
+    },
+
 });
 
 export default styles;
