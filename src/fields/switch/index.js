879351f7b2f00c930c50c8b1803f5c8c529a9410
krupanand <krupa@krupas-MacBook-Pro.local>
2022-01-05 15:59:06 +0530

    Update Switch field item and listitem to view

--- src/fields/switch/index.js
+++ src/fields/switch/index.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import { View, Text, Switch ,Item,Icon, ListItem} from "native-base";
+import { View, Text, Switch, Icon} from "native-base";
 import StarIcon from "../../components/starIcon"
 
 export default class SwitchField extends Component {
@@ -21,13 +21,12 @@ export default class SwitchField extends Component {
         const { attributes, theme, ErrorComponent } = this.props;
             return (
                 <View>
-                    <ListItem style={{ borderBottomWidth: 0, paddingVertical: 5 }}>
+                    <View style={{ borderBottomWidth: 0, paddingVertical: 5, paddingTop: 15, paddingHorizontal: 15, }}>
                         <View style={{ flex: 1}}>
-                            <View>
-                                <Item error={theme.changeTextInputColorOnError ? attributes.error : null}>
+                            <View style={{borderBottomWidth: 1,borderBottomColor: theme.inputColorPlaceholder,flexDirection:'row'}}>
                                     {attributes['required'] && <StarIcon required={attributes['required']} />}
                                     <View style={{flex:1 }}>
-                                        <Text style={{ color:theme.inputColorPlaceholder,padding:5 }}>
+                                        <Text style={{ fontSize:18, color:theme.inputColorPlaceholder,padding:5 }}>
                                             {attributes.label}
                                         </Text>
                                     </View>
@@ -35,11 +34,10 @@ export default class SwitchField extends Component {
                                         onTrackColor={"blue"}
                                         onValueChange={value => this.handleChange(value)}
                                         value={attributes.value} />
-                                    {theme.textInputErrorIcon && attributes.error ? <Icon name={theme.textInputErrorIcon} /> : null}
-                                </Item>
+                                    {/* {theme.textInputErrorIcon && attributes.error ? <Icon name={theme.textInputErrorIcon} /> : null} */}
                             </View>
                         </View>
-                    </ListItem>
+                    </View>
                     <View style={{ paddingHorizontal: 15 }}>
                         <ErrorComponent {...{ attributes, theme }} />
                     </View>
