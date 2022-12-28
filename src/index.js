658f1e685842e0fa4be3b6eebcaa46109dfe4b50
krupanand <krupa@krupas-MacBook-Pro.local>
2022-12-28 12:08:53 +0530

    Fix undefined key in payload

--- src/index.js
+++ src/index.js
@@ -405,7 +405,7 @@ export default class Form0 extends Component {
     const values = {};
     let isValidFields = true;
     Object.keys(this.state).forEach((fieldName) => {
-      const field = this.state[fieldName];
+      const field = fieldName !== 'calcFields' && this.state[fieldName];
       if (field) {
         if (field.error !== undefined && field.error) {
           isValidFields = false;
